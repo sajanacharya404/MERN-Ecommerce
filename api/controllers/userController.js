e27ee5bef66fd7839180de78a963128e8a4a06f6
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-09 13:55:01 +0545

    loginUser and getUser Api created

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -25,3 +25,34 @@ export const createUser = asyncHandler(async (req, res, next) => {
     throw new Error("Invalid user data");
   }
 });
+
+export const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if ((!email, !password)) {
+    throw new Error("Please fill all the inputs.");
+  }
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) return res.status(400).json("User doesnot exists");
+  const comparePassword = bcrypt.compareSync(password, existingUser.password);
+  if (comparePassword) {
+    createToken(res, existingUser._id);
+    res.status(200).json({
+      _id: existingUser._id,
+      name: existingUser.email,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+    return;
+  }
+});
+
+export const getAllUser = asyncHandler(async (req, res, next) => {
+  const userData = await User.find();
+  res.status(200).json({
+    _id: userData._id,
+    name: userData.email,
+    email: userData.email,
+    isAdmin: userData.isAdmin,
+  });
+});
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUser,
+  loginUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/users", createUser);
+router.post("/auth", loginUser);
+router.get("/users", getAllUser);
 export default router;
