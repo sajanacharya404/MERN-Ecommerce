254544d2a2aa745091d1920407b90123c2a8d511
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-27 15:18:57 +0545

    register pages updated

--- client/src/pages/Registerpage.jsx
+++ client/src/pages/Registerpage.jsx
@@ -1,85 +1,74 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 
 const Registerpage = () => {
-  const [userData, setUserData] = useState({});
-  const [error, setError] = useState()
+  const [userData, setUserData] = useState("");
 
   const handleChange = (e) => {
     setUserData({ ...userData, [e.target.id]: e.target.value });
   };
-
+  console.log(userData);
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("/api/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
-
-      const data = await res.json();
-      if(!data.success){
-        setError(data.message);
-      }
-      setError(data.message);
-    } catch (error) {setError(error.message)}
+      await axios.post("/api/users", userData);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className='h-[93vh] items-center flex flex-col justify-center  gap-2 bg-slate-300 '>
+    <div className="h-[93vh] items-center flex flex-col justify-center  gap-2 bg-slate-300 ">
       <form
-        className=' flex flex-col gap-5 items-center h-96 p-10 '
+        className=" flex flex-col gap-5 items-center h-96 p-10 "
         onSubmit={handleSubmit}
       >
-        <div className='flex flex-col'>
-          <label htmlFor='username' className=' text-xl'>
+        <div className="flex flex-col">
+          <label htmlFor="name" className=" text-xl">
             Username
           </label>
           <input
-            id='username'
-            type='text'
-            placeholder='enter your username..'
-            className='border-2 border-slate-900 outline-none p-2 rounded-md'
+            id="name"
+            type="text"
+            placeholder="enter your username.."
+            className="border-2 border-slate-900 outline-none p-2 rounded-md"
             onChange={handleChange}
             required
           />
         </div>
-        <div className='flex flex-col'>
-          <label htmlFor='email' className=' text-xl'>
+        <div className="flex flex-col">
+          <label htmlFor="email" className=" text-xl">
             Email
           </label>
           <input
-            id='email'
-            type='email'
-            placeholder='enter your email..'
-            className='border-2 border-slate-900 outline-none p-2 rounded-md'
+            id="email"
+            type="email"
+            placeholder="enter your email.."
+            className="border-2 border-slate-900 outline-none p-2 rounded-md"
             onChange={handleChange}
             required
           />
         </div>
-        <div className='flex flex-col'>
-          <label htmlFor='password' className=' text-xl'>
+        <div className="flex flex-col">
+          <label htmlFor="password" className=" text-xl">
             Password
           </label>
           <input
-            id='password'
-            placeholder='enter your password..'
-            className='border-2 border-slate-900 outline-none p-2 rounded-md'
+            id="password"
+            placeholder="enter your password.."
+            className="border-2 border-slate-900 outline-none p-2 rounded-md"
             onChange={handleChange}
           />
         </div>
-        <button className='bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800'>
+        <button className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800">
           Register
         </button>
       </form>
-      <p className=''>
+      <p className="">
         Already have an account?
-        <span className='text-blue-900 underline p-2 '>
-          <Link to='/login'>Login</Link>
+        <span className="text-blue-900 underline p-2 ">
+          <Link to="/login">Login</Link>
         </span>
       </p>
     </div>
